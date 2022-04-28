20da6a56c5fe9bfcf63e55e9ca4fd8ac34d90cf4
Med Beriri <mehdiberriri1@gmail.com>
2022-04-28 00:12:20 +0000

    User Details Service Security implemented

--- patient-mvc/src/main/java/com/emsi/patientsmvc/PatientsMvcApplication.java
+++ patient-mvc/src/main/java/com/emsi/patientsmvc/PatientsMvcApplication.java
@@ -2,11 +2,14 @@ package com.emsi.patientsmvc;
 
 import com.emsi.patientsmvc.entities.Patient;
 import com.emsi.patientsmvc.repositories.PatientRepository;
+import com.emsi.patientsmvc.security.services.SecurityService;
 import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -15,6 +18,10 @@ public class PatientsMvcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PatientsMvcApplication.class, args);
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
     @Bean
 CommandLineRunner commandLineRunner(PatientRepository PR){
@@ -31,6 +38,24 @@ CommandLineRunner commandLineRunner(PatientRepository PR){
             PR.findAll().forEach(patient -> {
                 System.out.println(patient.getNom());
             });
+        };
+}
+//@Bean
+CommandLineRunner saveUsers(SecurityService securityService){
+        return args -> {
+            securityService.saveNewUser("slimani","benimellal","benimellal");
+            securityService.saveNewUser("dahmad","jdida","jdida");
+            securityService.saveNewUser("slimaniya","casa","casa");
+
+            securityService.saveNewRole("USER","");
+            securityService.saveNewRole("ADMIN","");
+
+            securityService.addRoleToUser("slimani","USER");
+            securityService.addRoleToUser("slimani","ADMIN");
+
+
+            securityService.addRoleToUser("slimaniya","USER");
+            securityService.addRoleToUser("dahmad","USER");
         };
 }
 }
--- patient-mvc/src/main/java/com/emsi/patientsmvc/security/SecurityConfig.java
+++ patient-mvc/src/main/java/com/emsi/patientsmvc/security/SecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -21,20 +24,27 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder passwordEncoder=passwordEncoder();
+
         String encodedPWD =passwordEncoder.encode("1234");
         System.out.println(encodedPWD);
         /*auth.inMemoryAuthentication().withUser("user1").password(encodedPWD).roles("USER");
         auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1134")).roles("USER");
         auth.inMemoryAuthentication().withUser("Admin").password(passwordEncoder.encode("1224")).roles("USER","ADMIN");*/
-        auth.jdbcAuthentication()
+       /* auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .usersByUsernameQuery("select username as principal,password as credentials,active from users where username=?").
                 authoritiesByUsernameQuery("select username as principal,role as role from users_roles where username=?")
                 .rolePrefix("ROLE_")
-                .passwordEncoder(passwordEncoder);
+                .passwordEncoder(passwordEncoder);*/
+        auth.userDetailsService(userDetailsService);
+
 
     }
     @Override
@@ -47,13 +57,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
        http.formLogin();
        http.authorizeRequests().antMatchers("/").permitAll();
-       http.authorizeRequests().antMatchers("/user/**").hasRole("USER");
-       http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+       http.authorizeRequests().antMatchers("/user/**").hasAuthority("USER");
+       http.authorizeRequests().antMatchers("/admin/**").hasAuthority("ADMIN");
        http.authorizeRequests().anyRequest().authenticated();
        http.exceptionHandling().accessDeniedPage("/403");
     }
-@Bean
-   PasswordEncoder passwordEncoder(){
-return new BCryptPasswordEncoder();
-   }
+
 }
